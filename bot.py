37caf91e6395af870146b7f472457637707ee261
asbhaibsr <ffloverisarsad@gmail.com>
2025-06-18 08:59:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,4 +10,12 @@ keep_alive()
 
 Bot = Client("SonaAI", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-@Bot.on_message(filters.text & filters.group & ~filters.command(["start",]()_
+@Bot.on_message(filters.text & filters.group & ~filters.command(["start", "help", "settings"]))
+async def ai_chat(client, message: Message):
+    reply = await get_similar_reply(message.text)
+    if not reply:
+        reply = await generate_ai_reply(message.text)
+        await save_chat(message.from_user.id, message.text, reply)
+    await message.reply_text(reply)
+
+Bot.run()
