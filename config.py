23443ba464dfda6663e27b1229c0ce979b2b3346
asbhaibsr <ffloverisarsad@gmail.com>
2025-06-18 08:55:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-# config.py
 API_ID = 29970536
 API_HASH = "f4bfdcdd4a5c1b7328a7e4f25f024a09"
-BOT_TOKEN = "YOUR_BOT_TOKEN"  # Replace with your real token
-OWNER_ID = 7315805581
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Replace with actual token
 MONGO_URI = "mongodb+srv://wtqf35lojv:9uhGrKZE4i0zz05x@cluster0.nmtfsys.mongodb.net/?retryWrites=true&w=majority&tls=true"
-DB_NAME = "bot_db"
-COLLECTION_NAME = "chats"
