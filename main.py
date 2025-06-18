7d2d65875fda1f68bb3d616d642b7310d90ddbaa
asbhaibsr <ffloverisarsad@gmail.com>
2025-06-18 08:56:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from bot import bot
-from keep_alive import keep_alive
-import asyncio
-from pyrogram.idle import idle
-
-keep_alive()
-
-async def main():
-    await bot.start()
-    print("🤖 Bot is running...")
-    await idle()
-    await bot.stop()
+from bot import Bot
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    Bot.run()
