3f551aa5ce0083d80f286d917cddfe9cff012ae8
asbhaibsr <ffloverisarsad@gmail.com>
2025-06-18 08:55:35 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,23 +1,17 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URI, DB_NAME, COLLECTION_NAME
-from datetime import datetime, timedelta
+from config import MONGO_URI
 
 client = AsyncIOMotorClient(MONGO_URI)
-db = client[DB_NAME]
-collection = db[COLLECTION_NAME]
+db = client.sona_ai
+collection = db.user_chats
 
-async def save_chat(user_id, msg, reply):
+async def save_chat(user_id, user_message, bot_reply):
     await collection.insert_one({
         "user_id": user_id,
-        "msg": msg,
-        "reply": reply,
-        "time": datetime.utcnow()
+        "message": user_message,
+        "reply": bot_reply
     })
 
-async def get_similar_reply(user_msg):
-    one_day_ago = datetime.utcnow() - timedelta(days=1)
-    cursor = collection.find({"time": {"$gte": one_day_ago}})
-    async for doc in cursor:
-        if doc["msg"].lower() in user_msg.lower():
-            return doc["reply"]
-    return None
+async def get_similar_reply(message):
+    result = await collection.find_one({"message": message})
+    return result['reply'] if result else None
